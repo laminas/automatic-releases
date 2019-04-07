c2dbb0470dcd42f2cf9c4b011b157bdb622f0bb4
Marco Pivetta <ocramius@gmail.com>
2019-04-07 18:46:43 +0200

    Corrected CLI entry-point script namespace
    This file was initially copied over from `roave/security-advisories-builder`

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Roave\SecurityAdvisories;
+namespace Doctrine\AutomaticReleases\WebApplication;
 
 use Assert\Assert;
 use Doctrine\AutomaticReleases\Environment\Variables;
