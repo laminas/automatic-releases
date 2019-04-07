46524e4f2302959044356bcdde5d7d733d8aef86
Marco Pivetta <ocramius@gmail.com>
2019-04-07 12:47:36 +0200

    Abstraction for an issue/pull-request from a partial GraphQL API response

---
+++ src/Github/Api/GraphQL/Query/GetMilestoneChangelog/Response/IssueOrPullRequest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\AutomaticReleases\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response;
+
+use Assert\Assert;
+use Psr\Http\Message\UriInterface;
+use Zend\Diactoros\Uri;
+
+final class IssueOrPullRequest
+{
+    /** @var int */
+    private $number;
+
+    /** @var string */
+    private $title;
+
+    /** @var Author */
+    private $author;
+
+    /** @var array<int, Label> */
+    private $labels;
+
+    /** @var bool */
+    private $closed;
+
+    /** @var UriInterface */
+    private $url;
+
+    private function __construct()
+    {
+    }
+
+    public static function make(array $payload) : self
+    {
+        Assert::that($payload)
+              ->keyExists('number')
+              ->keyExists('title')
+              ->keyExists('author')
+              ->keyExists('url')
+              ->keyExists('closed')
+              ->keyExists('labels');
+
+        Assert::that($payload['number'])
+              ->integer()
+              ->greaterThan(0);
+
+        Assert::that($payload['title'])
+              ->string()
+              ->notEmpty();
+
+        Assert::that($payload['author'])
+              ->isArray();
+
+        Assert::that($payload['labels'])
+              ->isArray()
+              ->keyExists('nodes');
+
+        Assert::that($payload['labels']['nodes'])
+              ->isArray();
+
+        Assert::that($payload['url'])
+              ->string()
+              ->notEmpty();
+
+        $instance = new self();
+
+        $instance->number = $payload['number'];
+        $instance->title  = $payload['title'];
+        $instance->author = Author::make($payload['author']);
+        $instance->labels = array_values(array_map([Label::class, 'make'], $payload['labels']['nodes']));
+        $instance->url    = new Uri($payload['url']);
+        $instance->closed = isset($payload['merged'])
+            ? $payload['merged'] || $payload['closed']
+            : $payload['closed'];
+
+        return $instance;
+    }
+
+    public function number() : int
+    {
+        return $this->number;
+    }
+
+    public function title() : string
+    {
+        return $this->title;
+    }
+
+    public function author() : Author
+    {
+        return $this->author;
+    }
+
+    /** @return array<int, Label> */
+    public function labels() : array
+    {
+        return $this->labels;
+    }
+
+    public function closed() : bool
+    {
+        return $this->closed;
+    }
+
+    public function url() : UriInterface
+    {
+        return $this->url;
+    }
+}
---
+++ test/unit/Github/Api/GraphQL/Query/GetMilestoneChangelog/Response/IssueOrPullRequestTest.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\AutomaticReleases\Test\Unit\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response;
+
+use Doctrine\AutomaticReleases\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response\Author;
+use Doctrine\AutomaticReleases\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response\IssueOrPullRequest;
+use Doctrine\AutomaticReleases\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response\Label;
+use PHPUnit\Framework\TestCase;
+
+final class IssueOrPullRequestTest extends TestCase
+{
+    public function testIssue()
+    {
+        $issue = IssueOrPullRequest::make([
+            'number' => 123,
+            'title'  => 'Yadda',
+            'author' => [
+                'login' => 'Magoo',
+                'url'   => 'http://example.com/author',
+            ],
+            'url'    => 'http://example.com/issue',
+            'closed' => true,
+            'labels' => [
+                'nodes' => [
+                    [
+                        'name'  => 'BC Break',
+                        'color' => 'abcabc',
+                        'url'   => 'http://example.com/bc-break',
+                    ],
+                    [
+                        'name'  => 'Question',
+                        'color' => 'defdef',
+                        'url'   => 'http://example.com/question',
+                    ],
+                ],
+            ],
+        ]);
+
+        self::assertSame(123, $issue->number());
+        self::assertTrue($issue->closed());
+        self::assertSame('http://example.com/issue', $issue->url()->__toString());
+        self::assertSame('Yadda', $issue->title());
+        self::assertEquals(
+            Author::make([
+                'login' => 'Magoo',
+                'url'   => 'http://example.com/author',
+            ]),
+            $issue->author()
+        );
+        self::assertEquals(
+            [
+                Label::make([
+                    'name'  => 'BC Break',
+                    'color' => 'abcabc',
+                    'url'   => 'http://example.com/bc-break',
+                ]),
+                Label::make([
+                    'name'  => 'Question',
+                    'color' => 'defdef',
+                    'url'   => 'http://example.com/question',
+                ]),
+            ],
+            $issue->labels()
+        );
+    }
+
+    public function testPullRequest()
+    {
+        $issue = IssueOrPullRequest::make([
+            'number' => 123,
+            'title'  => 'Yadda',
+            'author' => [
+                'login' => 'Magoo',
+                'url'   => 'http://example.com/author',
+            ],
+            'url'    => 'http://example.com/issue',
+            'closed' => false,
+            'merged' => true,
+            'labels' => [
+                'nodes' => [
+                    [
+                        'name'  => 'BC Break',
+                        'color' => 'abcabc',
+                        'url'   => 'http://example.com/bc-break',
+                    ],
+                    [
+                        'name'  => 'Question',
+                        'color' => 'defdef',
+                        'url'   => 'http://example.com/question',
+                    ],
+                ],
+            ],
+        ]);
+
+        self::assertSame(123, $issue->number());
+        self::assertTrue($issue->closed());
+        self::assertSame('http://example.com/issue', $issue->url()->__toString());
+        self::assertSame('Yadda', $issue->title());
+        self::assertEquals(
+            Author::make([
+                'login' => 'Magoo',
+                'url'   => 'http://example.com/author',
+            ]),
+            $issue->author()
+        );
+        self::assertEquals(
+            [
+                Label::make([
+                    'name'  => 'BC Break',
+                    'color' => 'abcabc',
+                    'url'   => 'http://example.com/bc-break',
+                ]),
+                Label::make([
+                    'name'  => 'Question',
+                    'color' => 'defdef',
+                    'url'   => 'http://example.com/question',
+                ]),
+            ],
+            $issue->labels()
+        );
+    }
+}
