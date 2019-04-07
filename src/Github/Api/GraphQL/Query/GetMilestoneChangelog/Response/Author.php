6d6a61e55a05ff92df2cbfafa2d438d02bbfe1c4
Marco Pivetta <ocramius@gmail.com>
2019-04-07 12:37:48 +0200

    Abstraction for an "author" fetched from a partial github GraphQL object

---
+++ src/Github/Api/GraphQL/Query/GetMilestoneChangelog/Response/Author.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\AutomaticReleases\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response;
+
+use Assert\Assert;
+use Psr\Http\Message\UriInterface;
+use Zend\Diactoros\Uri;
+
+final class Author
+{
+    /** @var string */
+    private $name;
+
+    /** @var Uri */
+    private $url;
+
+    private function __construct()
+    {
+    }
+
+    public static function make(array $payload) : self
+    {
+        Assert::that($payload)
+              ->keyExists('login')
+              ->keyExists('url');
+
+        Assert::that($payload['login'])
+              ->string()
+              ->notEmpty();
+
+        Assert::that($payload['url'])
+              ->string()
+              ->notEmpty();
+
+        $instance = new self();
+
+        $instance->name   = $payload['login'];
+        $instance->url    = new Uri($payload['url']);
+
+        return $instance;
+    }
+
+    public function name() : string
+    {
+        return $this->name;
+    }
+
+    public function url() : UriInterface
+    {
+        return $this->url;
+    }
+}
---
+++ test/unit/Github/Api/GraphQL/Query/GetMilestoneChangelog/Response/AuthorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\AutomaticReleases\Test\Unit\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response;
+
+use Doctrine\AutomaticReleases\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response\Author;
+use PHPUnit\Framework\TestCase;
+
+final class AuthorTest extends TestCase
+{
+    public function test()
+    {
+        $author = Author::make([
+            'login' => 'Magoo',
+            'url'   => 'http://example.com/',
+        ]);
+
+        self::assertSame('Magoo', $author->name());
+        self::assertSame('http://example.com/', $author->url()->__toString());
+    }
+}
