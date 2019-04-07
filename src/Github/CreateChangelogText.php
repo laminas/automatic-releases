a53bf625f2f38da993f9044459c97d9a75c203b7
Marco Pivetta <ocramius@gmail.com>
2019-04-07 14:25:11 +0200

    Utility to turn a retrieved `Milestone` into release notes

---
+++ src/Github/CreateChangelogText.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\AutomaticReleases\Github;
+
+use Doctrine\AutomaticReleases\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response\IssueOrPullRequest;
+use Doctrine\AutomaticReleases\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response\Label;
+use Doctrine\AutomaticReleases\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response\Milestone;
+use Psr\Http\Message\UriInterface;
+
+final class CreateChangelogText
+{
+    private const TEMPLATE = <<<'MARKDOWN'
+Release %release%
+
+%description%
+
+Closed issues:
+
+ * %closedIssues%
+
+MARKDOWN;
+
+    public function __invoke(Milestone $milestone) : string
+    {
+        $replacements = [
+            '%release%'      => $this->markdownLink($milestone->title(), $milestone->url()),
+            '%description%'  => $milestone->description(),
+            '%closedIssues%' => implode("\n * ", array_map([$this, 'entryToRow'], $milestone->entries())),
+        ];
+
+        return str_replace(
+            array_keys($replacements),
+            $replacements,
+            self::TEMPLATE
+        );
+    }
+
+    private function entryToRow(IssueOrPullRequest $issueOrPullRequest) : string
+    {
+        $author = $issueOrPullRequest->author();
+
+        return implode(' ', array_map([$this, 'labelToString'], $issueOrPullRequest->labels()))
+            . ' '
+            . $this->markdownLink($issueOrPullRequest->title(), $issueOrPullRequest->url())
+            . ' thanks to '
+            . $this->markdownLink($author->name(), $author->url());
+    }
+
+    private function labelToString(Label $label) : string
+    {
+        return '\\[' . $label->name() . '\\]';
+    }
+
+    private function markdownLink(string $text, UriInterface $uri) : string
+    {
+        return '[' . $text . '](' . $uri->__toString() . ')';
+    }
+}
---
+++ test/unit/Github/CreateChangelogTextTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\AutomaticReleases\Test\Unit\Github;
+
+use Doctrine\AutomaticReleases\Github\Api\GraphQL\Query\GetMilestoneChangelog\Response\Milestone;
+use Doctrine\AutomaticReleases\Github\CreateChangelogText;
+use PHPUnit\Framework\TestCase;
+
+final class CreateChangelogTextTest extends TestCase
+{
+    public function testGeneratedReleaseText()
+    {
+        self::assertSame(
+            <<<'RELEASE'
+Release [The title](http://example.com/milestone)
+
+The description
+
+Closed issues:
+
+ *  [Issue](http://example.com/issue) thanks to [Magoo](http://example.com/author)
+ *  [PR](http://example.com/issue) thanks to [Magoo](http://example.com/author)
+
+RELEASE
+            ,
+            (new CreateChangelogText())
+                ->__invoke(Milestone::make([
+                    'number'       => 123,
+                    'closed'       => true,
+                    'title'        => 'The title',
+                    'description'  => 'The description',
+                    'issues'       => [
+                        'nodes' => [
+                            [
+                                'number' => 456,
+                                'title'  => 'Issue',
+                                'author' => [
+                                    'login' => 'Magoo',
+                                    'url'   => 'http://example.com/author',
+                                ],
+                                'url'    => 'http://example.com/issue',
+                                'closed' => true,
+                                'labels' => [
+                                    'nodes' => [],
+                                ],
+                            ],
+                        ],
+                    ],
+                    'pullRequests' => [
+                        'nodes' => [
+                            [
+                                'number' => 789,
+                                'title'  => 'PR',
+                                'author' => [
+                                    'login' => 'Magoo',
+                                    'url'   => 'http://example.com/author',
+                                ],
+                                'url'    => 'http://example.com/issue',
+                                'merged' => true,
+                                'closed' => false,
+                                'labels' => [
+                                    'nodes' => [],
+                                ],
+                            ],
+                        ],
+                    ],
+                    'url'          => 'http://example.com/milestone',
+                ]))
+        );
+    }
+}
