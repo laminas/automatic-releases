716bb932469e3c1f22ee019cc0fa3742d71f9016
Marco Pivetta <ocramius@gmail.com>
2019-11-10 13:32:31 +0100

    Correct signature for global error handler in `index.php`

--- public/index.php
+++ public/index.php
@@ -48,7 +48,7 @@ use function uniqid;
     require_once __DIR__ . '/../vendor/autoload.php';
 
     set_error_handler(
-        static function ($errorCode, $message = '', $file = '', $line = 0) : void {
+        static function ($errorCode, $message = '', $file = '', $line = 0) : bool {
             throw new ErrorException($message, 0, $errorCode, $file, $line);
         },
         E_STRICT | E_NOTICE | E_WARNING
