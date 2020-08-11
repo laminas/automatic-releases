fcbeb7487045e7823a2676ad1ae8920fdf50b3b7
Matthew Weier O'Phinney <matthew@weierophinney.net>
2020-08-11 15:41:13 -0500

    feat: add BumpChangelogForReleaseBranch command
    Command determines the name of the release from the closed milestone,
    and uses it to bump the changelog version, commit, and push back to the
    associated release branch.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

---
+++ src/Application/Command/BumpChangelogForReleaseBranch.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\AutomaticReleases\Application\Command;
+
+use Laminas\AutomaticReleases\Changelog\BumpAndCommitChangelogVersion;
+use Laminas\AutomaticReleases\Environment\Variables;
+use Laminas\AutomaticReleases\Git\Fetch;
+use Laminas\AutomaticReleases\Git\GetMergeTargetCandidateBranches;
+use Laminas\AutomaticReleases\Github\Event\Factory\LoadCurrentGithubEvent;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Webmozart\Assert\Assert;
+
+use function sprintf;
+
+final class BumpChangelogForReleaseBranch extends Command
+{
+    private Variables $environment;
+    private LoadCurrentGithubEvent $loadEvent;
+    private Fetch $fetch;
+    private GetMergeTargetCandidateBranches $getMergeTargets;
+    private BumpAndCommitChangelogVersion $bumpChangelogVersion;
+
+    public function __construct(
+        Variables $environment,
+        LoadCurrentGithubEvent $loadEvent,
+        Fetch $fetch,
+        GetMergeTargetCandidateBranches $getMergeTargets,
+        BumpAndCommitChangelogVersion $bumpChangelogVersion
+    ) {
+        parent::__construct('laminas:automatic-releases:bump-changelog');
+
+        $this->environment          = $environment;
+        $this->loadEvent            = $loadEvent;
+        $this->fetch                = $fetch;
+        $this->getMergeTargets      = $getMergeTargets;
+        $this->bumpChangelogVersion = $bumpChangelogVersion;
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $milestoneClosedEvent = ($this->loadEvent)();
+        $repositoryName       = $milestoneClosedEvent->repository();
+        $repositoryCloneUri   = $repositoryName->uriWithTokenAuthentication($this->environment->githubToken());
+        $repositoryPath       = $this->environment->githubWorkspacePath();
+
+        Assert::directory($repositoryPath . '/.git');
+
+        ($this->fetch)($repositoryCloneUri, $repositoryPath);
+
+        $mergeCandidates = ($this->getMergeTargets)($repositoryPath);
+        $releaseVersion  = $milestoneClosedEvent->version();
+        $releaseBranch   = $mergeCandidates->targetBranchFor($releaseVersion);
+
+        Assert::notNull(
+            $releaseBranch,
+            sprintf('No valid release branch found for version %s', $releaseVersion->fullReleaseName())
+        );
+
+        ($this->bumpChangelogVersion)(
+            BumpAndCommitChangelogVersion::BUMP_PATCH,
+            $repositoryPath,
+            $releaseVersion,
+            $releaseBranch
+        );
+
+        return 0;
+    }
+}
---
+++ test/unit/Application/BumpChangelogForReleaseBranchTest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\AutomaticReleases\Test\Unit\Application;
+
+use Laminas\AutomaticReleases\Application\Command\BumpChangelogForReleaseBranch;
+use Laminas\AutomaticReleases\Changelog\BumpAndCommitChangelogVersion;
+use Laminas\AutomaticReleases\Environment\Variables;
+use Laminas\AutomaticReleases\Git\Fetch;
+use Laminas\AutomaticReleases\Git\GetMergeTargetCandidateBranches;
+use Laminas\AutomaticReleases\Git\Value\BranchName;
+use Laminas\AutomaticReleases\Git\Value\MergeTargetCandidateBranches;
+use Laminas\AutomaticReleases\Git\Value\SemVerVersion;
+use Laminas\AutomaticReleases\Github\Event\Factory\LoadCurrentGithubEvent;
+use Laminas\AutomaticReleases\Github\Event\MilestoneClosedEvent;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+
+use function mkdir;
+use function sys_get_temp_dir;
+use function tempnam;
+use function unlink;
+
+class BumpChangelogForReleaseBranchTest extends TestCase
+{
+    /** @var Variables&MockObject */
+    private $environment;
+    /** @var LoadCurrentGithubEvent&MockObject */
+    private $loadEvent;
+    /** @var Fetch&MockObject */
+    private $fetch;
+    /** @var GetMergeTargetCandidateBranches&MockObject */
+    private $getMergeTargets;
+    /** @var BumpAndCommitChangelogVersion&MockObject */
+    private $bumpChangelogVersion;
+    private MilestoneClosedEvent $event;
+    private BumpChangelogForReleaseBranch $command;
+
+    protected function setUp(): void
+    {
+        $this->environment          = $this->createMock(Variables::class);
+        $this->loadEvent            = $this->createMock(LoadCurrentGithubEvent::class);
+        $this->fetch                = $this->createMock(Fetch::class);
+        $this->getMergeTargets      = $this->createMock(GetMergeTargetCandidateBranches::class);
+        $this->bumpChangelogVersion = $this->createMock(BumpAndCommitChangelogVersion::class);
+
+        $this->command = new BumpChangelogForReleaseBranch(
+            $this->environment,
+            $this->loadEvent,
+            $this->fetch,
+            $this->getMergeTargets,
+            $this->bumpChangelogVersion
+        );
+
+        $this->event = MilestoneClosedEvent::fromEventJson(<<< 'JSON'
+            {
+                "milestone": {
+                    "title": "1.2.3",
+                    "number": 123
+                },
+                "repository": {
+                    "full_name": "foo/bar"
+                },
+                "action": "closed"
+            }
+            JSON);
+    }
+
+    public function testWillBumpChangelogVersion(): void
+    {
+        $workspace = tempnam(sys_get_temp_dir(), 'workspace');
+
+        unlink($workspace);
+        mkdir($workspace);
+        mkdir($workspace . '/.git');
+
+        $branches = MergeTargetCandidateBranches::fromAllBranches(
+            BranchName::fromName('1.1.x'),
+            BranchName::fromName('1.2.x'),
+            BranchName::fromName('1.3.x')
+        );
+
+        $this->loadEvent->method('__invoke')->willReturn($this->event);
+        $this->environment->method('githubToken')->willReturn('github-auth-token');
+        $this->environment->method('githubWorkspacePath')->willReturn($workspace);
+        $this->fetch->expects(self::once())
+            ->method('__invoke')
+            ->with(
+                'https://github-auth-token:x-oauth-basic@github.com/foo/bar.git',
+                $workspace
+            );
+        $this->getMergeTargets->expects(self::once())
+            ->method('__invoke')
+            ->with($workspace)
+            ->willReturn($branches);
+
+        $this->bumpChangelogVersion->expects(self::once())
+            ->method('__invoke')
+            ->with(
+                BumpAndCommitChangelogVersion::BUMP_PATCH,
+                $workspace,
+                self::equalTo(SemVerVersion::fromMilestoneName('1.2.3')),
+                self::equalTo(BranchName::fromName('1.2.x'))
+            );
+
+        self::assertSame(0, $this->command->run(new ArrayInput([]), new NullOutput()));
+    }
+}
