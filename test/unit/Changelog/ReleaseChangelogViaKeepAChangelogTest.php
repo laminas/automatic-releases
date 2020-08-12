8a7538b18cc700eaaf33b7f47321371a9e8d226f
Matthew Weier O'Phinney <matthew@weierophinney.net>
2020-08-12 15:24:12 -0500

    qa: ensure we set git author before committing in tests
    Prevents env issues during CI.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- test/unit/Changelog/ReleaseChangelogViaKeepAChangelogTest.php
+++ test/unit/Changelog/ReleaseChangelogViaKeepAChangelogTest.php
@@ -204,6 +204,8 @@ class ReleaseChangelogViaKeepAChangelogTest extends TestCase
         );
 
         (new Process(['git', 'init', '.'], $repo))->mustRun();
+        (new Process(['git', 'config', 'user.email', 'me@example.com'], $repo))->mustRun();
+        (new Process(['git', 'config', 'user.name', 'Just Me'], $repo))->mustRun();
         (new Process(['git', 'add', '.'], $repo))->mustRun();
         (new Process(['git', 'commit', '-m', 'Initial import'], $repo))->mustRun();
         (new Process(['git', 'switch', '-c', '1.0.x'], $repo))->mustRun();
