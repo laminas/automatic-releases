3abff1f403e14df1534464177cab5b5663c5385e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-04-11 19:08:05 -0400

    Add CreateTweetThroughApiCall
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Twitter/CreateTweetThroughApiCall.php
+++ src/Twitter/CreateTweetThroughApiCall.php
@@ -5,9 +5,46 @@ declare(strict_types=1);
 namespace Laminas\AutomaticReleases\Twitter;
 
 use Laminas\AutomaticReleases\Github\Event\MilestoneClosedEvent;
+use Laminas\AutomaticReleases\Twitter\Value\Tweet;
+use Laminas\Diactoros\Uri;
+use Laminas\Twitter\Response;
+use Laminas\Twitter\Twitter;
+use Psr\Http\Message\UriInterface;
+use stdClass;
+use Webmozart\Assert\Assert;
+
+use function sprintf;
+
 final class CreateTweetThroughApiCall
 {
+    private Twitter $twitter;
+
+    public function __construct(Twitter $twitter)
+    {
+        $this->twitter = $twitter;
+    }
+
     public function __invoke(MilestoneClosedEvent $event): UriInterface
     {
+        $tweet = Tweet::fromMilestoneClosedEvent($event);
+
+        $response = $this->statusesUpdate($tweet);
+
+        Assert::true($response->isSuccess());
+
+        $responseJson = $response->toValue();
+
+        Assert::isInstanceOf($responseJson, stdClass::class);
+        Assert::propertyExists($responseJson, 'id');
+
+        $tweetId = $responseJson->id;
+        Assert::integer($tweetId);
+
+        return new Uri(sprintf('https://twitter.com/i/web/status/%s', $tweetId));
+    }
+
+    private function statusesUpdate(Tweet $tweet): Response
+    {
+        return $this->twitter->statusesUpdate($tweet->content());
     }
 }
--- test/unit/Twitter/CreateTweetThroughApiCallTest.php
+++ test/unit/Twitter/CreateTweetThroughApiCallTest.php
@@ -4,7 +4,60 @@ declare(strict_types=1);
 
 namespace Laminas\AutomaticReleases\Test\Unit\Twitter;
 
+use Laminas\AutomaticReleases\Environment\EnvironmentVariables;
+use Laminas\AutomaticReleases\Github\Event\MilestoneClosedEvent;
+use Laminas\AutomaticReleases\Twitter\CreateTweetThroughApiCall;
+use Laminas\Diactoros\Uri;
+use Laminas\Http\Response as HttpResponse;
+use Laminas\Json\Json;
+use Laminas\Twitter\Response as TwitterResponse;
+use Laminas\Twitter\Twitter;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
 /** @covers Laminas\AutomaticReleases\Twitter\CreateTweetThroughApiCall */
 final class CreateTweetThroughApiCallTest extends TestCase
 {
+    private Twitter | MockObject $twitter;
+    private EnvironmentVariables | MockObject $variables;
+    private CreateTweetThroughApiCall | MockObject $createTweetThroughApiCall;
+    private MilestoneClosedEvent | MockObject $milestoneClosedEvent;
+    private HttpResponse | MockObject $httpResponse;
+    private TwitterResponse | MockObject $twitterResponse;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->variables = $this->createMock(EnvironmentVariables::class);
+
+        $this->milestoneClosedEvent = MilestoneClosedEvent::fromEventJson(
+            '{"milestone":{"title": "1.2.3","number": 123},"repository": {"full_name": "foo/bar"},"action": "closed"}'
+        );
+
+        $this->httpResponse = $this->createMock(HttpResponse::class);
+        $this->httpResponse->method('getContent')
+            ->willReturn('{"id":12345,"id_str":"12345"}');
+
+        $this->twitterResponse = $this->createMock(TwitterResponse::class);
+        $this->twitterResponse->method('isSuccess')->willReturn(true);
+        $this->twitterResponse->method('toValue')->willReturn(Json::decode(
+            (string) $this->httpResponse->getContent(),
+            Json::TYPE_OBJECT
+        ));
+
+        $this->twitter = $this->createMock(Twitter::class);
+        $this->twitter->method('statusesUpdate')->willReturn($this->twitterResponse);
+
+        $this->createTweetThroughApiCall = new CreateTweetThroughApiCall($this->twitter);
+    }
+
+    public function testSuccessfulRequest(): void
+    {
+        $tweetUri = ($this->createTweetThroughApiCall)($this->milestoneClosedEvent);
+
+        self::assertInstanceOf(Uri::class, $tweetUri);
+
+        self::assertSame('https://twitter.com/i/web/status/12345', $tweetUri->__toString());
+    }
 }
