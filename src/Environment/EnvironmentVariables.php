74011455e01d7324054b0be87a1eef4fc9451f1a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-24 10:51:23 -0400

    Refactor Twitter EnvironmentVariables
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Environment/EnvironmentVariables.php
+++ src/Environment/EnvironmentVariables.php
@@ -7,6 +7,7 @@ namespace Laminas\AutomaticReleases\Environment;
 use Laminas\AutomaticReleases\Environment\Traits\EnvTrait;
 use Laminas\AutomaticReleases\Gpg\ImportGpgKeyFromString;
 use Laminas\AutomaticReleases\Gpg\SecretKeyId;
+
 use function Psl\invariant;
 use function Psl\Iter\contains;
 
@@ -47,14 +48,6 @@ class EnvironmentVariables implements Variables
     private string $workspacePath;
     /** @psalm-var non-empty-string */
     private string $logLevel;
-    /** @psalm-var non-empty-string */
-    private string $twitterConsumerApiKey;
-    /** @psalm-var non-empty-string */
-    private string $twitterConsumerApiSecret;
-    /** @psalm-var non-empty-string */
-    private string $twitterAaccessToken;
-    /** @psalm-var non-empty-string */
-    private string $twitterAccessTokenSecret;
 
     /**
      * @psalm-param non-empty-string $githubToken
@@ -63,10 +56,6 @@ class EnvironmentVariables implements Variables
      * @psalm-param non-empty-string $githubEventPath
      * @psalm-param non-empty-string $workspacePath
      * @psalm-param non-empty-string $logLevel
-     * @psalm-param non-empty-string $twitterConsumerApiKey
-     * @psalm-param non-empty-string $twitterConsumerApiSecret
-     * @psalm-param non-empty-string $twitterAaccessToken
-     * @psalm-param non-empty-string $twitterAccessTokenSecret
      */
     private function __construct(
         string $githubToken,
@@ -75,22 +64,14 @@ class EnvironmentVariables implements Variables
         string $gitAuthorEmail,
         string $githubEventPath,
         string $workspacePath,
-        string $logLevel,
-        string $twitterAaccessToken,
-        string $twitterAccessTokenSecret,
-        string $twitterConsumerApiKey,
-        string $twitterConsumerApiSecret
+        string $logLevel
     ) {
-        $this->githubToken              = $githubToken;
-        $this->signingSecretKey         = $signingSecretKey;
-        $this->gitAuthorName            = $gitAuthorName;
-        $this->gitAuthorEmail           = $gitAuthorEmail;
-        $this->githubEventPath          = $githubEventPath;
-        $this->workspacePath            = $workspacePath;
-        $this->twitterConsumerApiKey    = $twitterConsumerApiKey;
-        $this->twitterConsumerApiSecret = $twitterConsumerApiSecret;
-        $this->twitterAaccessToken      = $twitterAaccessToken;
-        $this->twitterAccessTokenSecret = $twitterAccessTokenSecret;
+        $this->githubToken      = $githubToken;
+        $this->signingSecretKey = $signingSecretKey;
+        $this->gitAuthorName    = $gitAuthorName;
+        $this->gitAuthorEmail   = $gitAuthorEmail;
+        $this->githubEventPath  = $githubEventPath;
+        $this->workspacePath    = $workspacePath;
 
         /** @psalm-suppress ImpureFunctionCall the {@see \Psl\Iter\contains()} API is conditionally pure */
         invariant(
@@ -105,44 +86,14 @@ class EnvironmentVariables implements Variables
     public static function fromEnvironment(ImportGpgKeyFromString $importKey): self
     {
         return new self(
-            self::getenv('GITHUB_TOKEN'),
-            $importKey->__invoke(self::getenv('SIGNING_SECRET_KEY')),
-            self::getenv('GIT_AUTHOR_NAME'),
-            self::getenv('GIT_AUTHOR_EMAIL'),
-            self::getenv('GITHUB_EVENT_PATH'),
-            self::getenv('GITHUB_WORKSPACE'),
-            self::getenvWithFallback('LOG_LEVEL', 'INFO'),
-            self::getenv('TWITTER_CONSUMER_API_KEY'),
-            self::getenv('TWITTER_CONSUMER_API_SECRET'),
-            self::getenv('TWITTER_ACCESS_TOKEN'),
-            self::getenv('TWITTER_ACCESS_TOKEN_SECRET')
+            self::getEnv('GITHUB_TOKEN'),
+            $importKey->__invoke(self::getEnv('SIGNING_SECRET_KEY')),
+            self::getEnv('GIT_AUTHOR_NAME'),
+            self::getEnv('GIT_AUTHOR_EMAIL'),
+            self::getEnv('GITHUB_EVENT_PATH'),
+            self::getEnv('GITHUB_WORKSPACE'),
+            self::getenvWithFallback('LOG_LEVEL', 'INFO')
         );
-    }
-
-    /**
-     * @psalm-param  non-empty-string $key
-     *
-     * @psalm-return non-empty-string
-     */
-    private static function getenv(string $key): string
-    {
-        $value = Env\get_var($key);
-
-        Psl\invariant($value !== null && $value !== '', Str\format('Could not find a value for environment variable "%s"', $key));
-
-        return $value;
-    }
-
-    /**
-     * @psalm-param  non-empty-string $default
-     *
-     * @psalm-return non-empty-string
-     */
-    private static function getenvWithFallback(string $key, string $default): string
-    {
-        $value = Env\get_var($key);
-
-        return $value === null || $value === '' ? $default : $value;
     }
 
     public function githubToken(): string
@@ -179,25 +130,5 @@ class EnvironmentVariables implements Variables
     public function logLevel(): string
     {
         return $this->logLevel;
-    }
-
-    public function twitterConsumerApiKey(): string
-    {
-        return $this->twitterConsumerApiKey;
-    }
-
-    public function twitterConsumerApiSecret(): string
-    {
-        return $this->twitterConsumerApiSecret;
-    }
-
-    public function twitterAccessToken(): string
-    {
-        return $this->twitterAaccessToken;
-    }
-
-    public function twitterAccessTokenSecret(): string
-    {
-        return $this->twitterAccessTokenSecret;
     }
 }
