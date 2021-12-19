f53850111d6e886f50ea622a9402349cf1b16743
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-12-19 15:43:58 -0500

    Update PublishTweetTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/unit/Twitter/PublishTweetTest.php
+++ test/unit/Twitter/PublishTweetTest.php
@@ -16,6 +16,11 @@ use Laminas\AutomaticReleases\Twitter\Value\Tweet;
 use PHPUnit\Framework\MockObject\MockObject;
 use Throwable;
 
+use function Psl\Env\get_var;
+use function Psl\Env\remove_var;
+use function Psl\Env\set_var;
+use function Psl\SecureRandom\string;
+
 /**
  * @covers Laminas\AutomaticReleases\Twitter\PublishTweet
  * @psalm-suppress MissingConstructor
@@ -59,26 +64,56 @@ EOT;
             'url'          => 'https://github.com/vendor/project/releases/milestone/123',
         ]);
 
-        $this->twitter      = $this->createMock(TwitterOAuth::class);
-        $this->environment  = $this->createMock(Variables::class);
+        $this->twitter     = $this->createMock(TwitterOAuth::class);
+        $this->environment = $this->createMock(Variables::class);
+        $this->environment
+            ->expects(self::once())
+            ->method('twitterEnabled')
+            ->willReturnCallback(static fn (): bool => get_var('TWITTER_ACCESS_TOKEN') !== null);
+
         $this->tweet        = Tweet::fromMilestone($this->milestone);
         $this->publishTweet = new PublishTweet($this->twitter, $this->environment);
     }
 
-    public function testExtractsOnlyFirstTweet(): void
+    protected function tearDown(): void
     {
-        self::assertSame('My first tweet.', $this->tweet->__toString());
+        parent::tearDown();
+        $this->resetVars();
     }
 
     public function testSuccessfulRequest(): void
     {
+        $this->resetVars();
+
+        self::assertSame('My first tweet.', $this->tweet->__toString());
+
         $errors = 0;
         try {
             ($this->publishTweet)($this->tweet);
-        } catch (Throwable $exception) {
+        } catch (Throwable $_exception) {
             ++$errors;
         }
 
         self::assertSame(0, $errors, 'Error publishing a Tweet.');
+    }
+
+    public function testTwitterOAuthException(): void
+    {
+        set_var('TWITTER_ACCESS_TOKEN', 'twitterAccessToken' . string(8));
+        set_var('TWITTER_ACCESS_TOKEN_SECRET', 'twitterAccessTokenSecret' . string(8));
+        set_var('TWITTER_CONSUMER_API_KEY', 'twitterConsumerApiKey' . string(8));
+        set_var('TWITTER_CONSUMER_API_SECRET', 'twitterConsumerApiSecret' . string(8));
+
+        $this->expectException(TwitterOAuthException::class);
+
+        ($this->publishTweet)($this->tweet);
+    }
+
+    private function resetVars(): void
+    {
+        remove_var('TWITTER_ACCESS_TOKEN');
+        remove_var('TWITTER_ACCESS_TOKEN_SECRET');
+        remove_var('TWITTER_CONSUMER_API_KEY');
+        remove_var('TWITTER_CONSUMER_API_SECRET');
     }
 }
