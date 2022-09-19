6ab4bb0bb4dfc2e5e7ae13d0e45d9a3351f9dc4a
Marco Pivetta <ocramius@gmail.com>
2022-09-19 18:54:26 +0200

    Log processor to convert `HttpRequestInterface` instance into loggable strings
    This also considers the privacy/security concerns of **NOT** logging sensitive
    parameters.

---
+++ src/Monolog/ConvertLogContextHttpRequestsIntoStrings.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\AutomaticReleases\Monolog;
+
+use Monolog\LogRecord;
+use Monolog\Processor\ProcessorInterface;
+use Psr\Http\Message\RequestInterface;
+
+/** @internal */
+final class ConvertLogContextHttpRequestsIntoStrings implements ProcessorInterface
+{
+    public function __invoke(LogRecord $record): LogRecord
+    {
+        return new LogRecord(
+            $record->datetime,
+            $record->channel,
+            $record->level,
+            $record->message,
+            array_map(static function ($item): mixed {
+                if (! $item instanceof RequestInterface) {
+                    return $item;
+                }
+
+                return $item->getMethod()
+                    . ' '
+                    . $item
+                        ->getUri()
+                        ->withUserInfo('')
+                        ->__toString();
+            }, $record->context),
+            $record->extra,
+            $record->formatted
+        );
+    }
+}
