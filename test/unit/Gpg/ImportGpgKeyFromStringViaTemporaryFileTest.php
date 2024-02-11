0174cd0eac37156d698b602c0cb9cc78d9625d3a
Aleksei Khudiakov <aleksey@xerkus.pro>
2024-02-11 19:24:57 +1000

    Refactor gpg import to use machine readable colon format
    Signed-off-by: Aleksei Khudiakov <aleksey@xerkus.pro>

--- test/unit/Gpg/ImportGpgKeyFromStringViaTemporaryFileTest.php
+++ test/unit/Gpg/ImportGpgKeyFromStringViaTemporaryFileTest.php
@@ -7,6 +7,8 @@ namespace Laminas\AutomaticReleases\Test\Unit\Gpg;
 use Laminas\AutomaticReleases\Gpg\ImportGpgKeyFromStringViaTemporaryFile;
 use Laminas\AutomaticReleases\Gpg\SecretKeyId;
 use PHPUnit\Framework\TestCase;
+use Psl\Exception\InvariantViolationException;
+use Psl\Shell\Exception\FailedExecutionException;
 
 use function Psl\File\read;
 
@@ -20,5 +22,28 @@ final class ImportGpgKeyFromStringViaTemporaryFileTest extends TestCase
             (new ImportGpgKeyFromStringViaTemporaryFile())
                 ->__invoke(read(__DIR__ . '/../../asset/dummy-gpg-key.asc')),
         );
+    }
+
+    public function testWillImportGpgKeyWithValidSubkey(): void
+    {
+        self::assertEquals(
+            SecretKeyId::fromBase16String('8CA5C026AE941316'),
+            (new ImportGpgKeyFromStringViaTemporaryFile())
+                ->__invoke(read(__DIR__ . '/../../asset/dummy-gpg-only-subkey.asc')),
+        );
+    }
+
+    public function testWillFailOnNoSecretKey(): void
+    {
+        $this->expectException(InvariantViolationException::class);
+        $this->expectExceptionMessage('Imported GPG key material does not contain secret key');
+        (new ImportGpgKeyFromStringViaTemporaryFile())
+            ->__invoke(read(__DIR__ . '/../../asset/dummy-gpg-key-no-secret.asc'));
+    }
+
+    public function testWillFailOnInvalidGpgKey(): void
+    {
+        $this->expectException(FailedExecutionException::class);
+        (new ImportGpgKeyFromStringViaTemporaryFile())->__invoke('-----BEGIN PGP PRIVATE KEY BLOCK-----');
     }
 }
